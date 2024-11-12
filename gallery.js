74d254ab4cc5c6dfbe9b2d8f8788f0529f4b37a2
Gazzy21 <nicholas221@icloud.com>
2024-11-12 11:16:09 -0500

    Event Handlers for Navigation

--- gallery.js
+++ gallery.js
@@ -4,22 +4,26 @@ const mUrl = 'images.json' // Replace with actual JSON URL
 const mWaitTime = 5000 // Timer interval in milliseconds
 
 $(document).ready(() => {
-  $('#more').on('click', function() {
+
+
+  // Call a function here to start the timer for the slideshow
+
+  // Select the moreIndicator button and add a click event to:
+  // - toggle the rotation classes (rot90 and rot270)
+  // - slideToggle the visibility of the .details section
+  $('#more').on('click', function () {
     $('.details').slideToggle(); // Toggle visibility of .details on click
   });
-  
-// Call a function here to start the timer for the slideshow
-
-// Select the moreIndicator button and add a click event to:
-// - toggle the rotation classes (rot90 and rot270)
-// - slideToggle the visibility of the .details section
-
-// Select the "Next Photo" button and add a click event to call showNextPhoto
-
-// Select the "Previous Photo" button and add a click event to call showPrevPhoto
-
-// Call fetchJSON() to load the initial set of images
-fetchJSON()
+  // Select the "Next Photo" button and add a click event to call showNextPhoto
+  $('#nextPhoto').on('click', function () {
+    showNextPhoto()
+  })
+  // Select the "Previous Photo" button and add a click event to call showPrevPhoto
+  $('#prevPhoto').on('click', function () {
+    showPrevPhoto()
+  })
+  // Call fetchJSON() to load the initial set of images
+  fetchJSON()
 })
 
 // Function to fetch JSON data and store it in mImages
@@ -30,8 +34,7 @@ function fetchJSON() {
     url: mUrl,
     success: function (data) {
       mImages = data.images
-      console.log(mImages[0].imgPath)
-      $("#photo").attr('src', mImages[mCurrentIndex].imgPath)
+      swapPhoto()
 
     }
   });
@@ -44,17 +47,27 @@ function swapPhoto() {
   // Access mImages[mCurrentIndex] to update the image source and details
   // Update the #photo element's src attribute with the current image's path
   // Update the .location, .description, and .date elements with the current image's details
+  $("#photo").attr('src', mImages[mCurrentIndex].imgPath)
 }
 
 // Advances to the next photo, loops to the first photo if the end of array is reached
 function showNextPhoto() {
-  // Increment mCurrentIndex and call swapPhoto()
+  mCurrentIndex++;
+  if (mCurrentIndex > mImages.length-1) {
+    mCurrentIndex = 0
+  }
+  swapPhoto()
   // Ensure it loops back to the beginning if mCurrentIndex exceeds array length
 }
 
 // Goes to the previous photo, loops to the last photo if mCurrentIndex goes negative
 function showPrevPhoto() {
   // Decrement mCurrentIndex and call swapPhoto()
+  mCurrentIndex--;
+  if (mCurrentIndex < 0) {
+    mCurrentIndex = mImages.length-1
+  }
+  swapPhoto()
   // Ensure it loops to the end if mCurrentIndex is less than 0
 }
 
